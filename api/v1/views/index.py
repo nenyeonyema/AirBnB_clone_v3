306f6a2a0746e4faae2c795e965b10e95ed24374
nenyeonyema <chinenyeonyema1@gmail.com>
2024-03-03 15:23:50 -0800

    retrieve objects stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,17 @@ from flask import jsonify
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieve the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
