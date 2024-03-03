82a0cd4096218c69a166728ec9e11aa104ea956e
nenyeonyema <chinenyeonyema1@gmail.com>
2024-03-03 14:58:27 -0800

    api

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+# api/v1/app.py
+from flask import Flask
+from api.v1.views import app_views
+from models import storage
+import os
+
+app = Flask(__name__)
+
+# Register the blueprint
+app.register_blueprint(app_views)
+
+
+# Define a method to handle teardown app context
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Closes the storage connection"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    # Run the Flask server
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
