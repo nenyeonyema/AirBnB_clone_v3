ed715fc6643292575b08454c972a3c7834bfd133
nenyeonyema <chinenyeonyema1@gmail.com>
2024-03-04 03:42:36 -0800

    check

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Defines the API routes for Amenity objects."""
-
+""" Defines the API routes for Amenity objects. """
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
@@ -9,14 +8,14 @@ from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
-    """Retrieves the list of all Amenity objects."""
+    """ Retrieves the list of all Amenity objects. """
     amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
     return jsonify(amenities)
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
-    """Retrieves a Amenity object."""
+    """ Retrieves a Amenity object. """
     amenity = storage.get(Amenity, amenity_id)
     if amenity:
         return jsonify(amenity.to_dict())
@@ -26,7 +25,7 @@ def get_amenity(amenity_id):
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
-    """Deletes a Amenity object."""
+    """ Deletes a Amenity object. """
     amenity = storage.get(Amenity, amenity_id)
     if amenity:
         storage.delete(amenity)
@@ -38,7 +37,7 @@ def delete_amenity(amenity_id):
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
-    """Creates a Amenity."""
+    """ Creates a Amenity. """
     if not request.json:
         abort(400, "Not a JSON")
     if 'name' not in request.json:
@@ -51,7 +50,7 @@ def create_amenity():
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
-    """Updates a Amenity object."""
+    """ Updates a Amenity object. """
     amenity = storage.get(Amenity, amenity_id)
     if amenity:
         data = request.json
