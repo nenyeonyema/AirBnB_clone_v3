8ef0456f2c37ee8aaca5025b158ac49c81b7c7a3
nenyeonyema <chinenyeonyema1@gmail.com>
2024-03-04 04:20:44 -0800

    updated

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Define a method to handle teardown app context """
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
+import os
+
+app = Flask(__name__)
+
+# Register the blueprint
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Closes the storage connection"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors."""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    # Run the Flask server
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+create a route /status on the object
+app_views that returns a JSON: "status": "OK"
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
